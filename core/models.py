3d4573bec8c30c4126eb1a9e20cba7ee2c993bb1
raalsky <raalsky@gmail.com>
2017-02-13 12:00:04 +0100

    Restructurized

--- core/models.py
+++ core/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-class Address(models.Model):
-    postalcode = models.CharField(db_column='PostalCode', primary_key=True, max_length=6)
-    province = models.CharField(db_column='Province', max_length=25)
-    city = models.CharField(db_column='City', max_length=25)
-
-    class Meta:
-        db_table = 'Addresses'
--- mps/settings.py
+++ mps/settings.py
@@ -32,9 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
-    'wholesales.apps.WholesalesConfig',
-    'pharmacies.apps.PharmaciesConfig',
-    'institutions.apps.InstitutionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
