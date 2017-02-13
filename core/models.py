74bf3bb21f01a5c663a6d1af76601a93426f31fc
raalsky <raalsky@gmail.com>
2017-02-13 11:57:18 +0100

    Core models modified

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    postalcode = models.CharField(db_column='PostalCode', primary_key=True, max_length=6)
+    province = models.CharField(db_column='Province', max_length=25)
+    city = models.CharField(db_column='City', max_length=25)
+
+    class Meta:
+        db_table = 'Addresses'
