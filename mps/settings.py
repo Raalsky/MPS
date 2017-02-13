8fb432f6456449c37671a91a998162f87e1e00c5
raalsky <raalsky@gmail.com>
2017-02-13 22:18:03 +0100

    Pharmacies models added

--- mps/settings.py
+++ mps/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main.apps.MainConfig',
     'wholesales.apps.WholesalesConfig',
+    'pharmacies.apps.PharmaciesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pharmacies/models.py
+++ pharmacies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pharmacy(models.Model):
+    Name                = models.CharField(max_length=50)
+    Type                = models.CharField(max_length=30)
+    AuthorizationNr     = models.CharField(max_length=40)
+    CreationDate        = models.DateField()
+    AuthorizationDate   = models.DateField()
+    PostalCode          = models.ForeignKey('main.PostalCode')
+    Street              = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'Pharmacies'
+
+class Pharmacist(models.Model):
+    UserId          = models.ForeignKey(User)
+    PharmacyId      = models.ForeignKey(Pharmacy)
+    EmploymentDate  = models.DateField()
+
+    class Meta:
+        db_table    = "Pharmacists"
+
+class PharmacyProduct(models.Model):
+    MedicineId      = models.ForeignKey('main.Medicine')
+    PharmacyId      = models.ForeignKey(Pharmacy)
+    Price           = models.FloatField()
+    class Meta:
+        db_table    = "PharmaciesProducts"
