f5037a530910e3270ee11643ef3e85830efecaca
raalsky <raalsky@gmail.com>
2017-02-21 22:34:30 +0100

    Login page template created

--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import *
+from django.shortcuts import render_to_response,redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    return render(request, 'main/login.html')
+
+"""@login_required(login_url='/login/')
+def main(request):
+    return """
--- mps/urls.py
+++ mps/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from main import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^', include('wholesales.urls')),
+    url(r'^login/$', views.login_user, name='login_user'),
 ]
